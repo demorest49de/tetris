2d066868191085ba6fe5897c2d64a46a94b4cde7
shevchenkoak <demorest49de@gmail.com>
2022-04-22 10:19:12 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,110 @@
+const _SIZEBLOCK = 30
+
+
+// mehanika
+
+const game = {
+    area: [
+        ['o','o','o','o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o','o','o','x'],
+        ['o','o','o','o','x','x','o','o','o','x'],
+        ['o','o','o','o','x','x','o','o','x','x']
+    ],
+
+    activeTetromino:{
+        x: 4,
+        y: 0,
+        block: [
+            ['o','x','o'],
+            ['o','x','o'],
+            ['x','x','o'],
+        ]
+    },
+
+    moveLeft(){
+        this.activeTetromino.x -= 1
+    },
+
+    moveRight(){
+        this.activeTetromino.x += 1
+    },
+
+    moveDown(){
+        this.activeTetromino.y += 1
+    },
+
+    rotateTetromino(){
+
+    },
+
+    viewArea(){
+        const area = JSON.parse(JSON.stringify(this.area))
+        const {x, y, block: tetromino} = this.activeTetromino
+        // console.log(tetromino)
+        for (let i = 0; i < tetromino.length; i++) {
+             const row = tetromino[i]
+            for (let j = 0; j < row.length; j++) {
+                if(row[j] === 'x'){
+                    area[y + i][x + j] = tetromino[i][j]
+                }
+            }
+        }
+        // console.log(area)
+    },
+}
+
+game.viewArea()
+
+
+
+// otrisovka
+const container = document.querySelector('.container')
+
+const canvas = document.createElement('canvas')
+canvas.classList.add('game-area')
+container.append(canvas)
+
+canvas.width = _SIZEBLOCK * 10
+canvas.height = _SIZEBLOCK * 20
+
+const context = canvas.getContext('2d')
+
+const showArea = area => {
+    for (let y = 0; y < area.length; y++) {
+        const line = area[y]
+
+        for (let x = 0; x < line.length; x++) {
+            const block = line[x]
+            if (block === 'x'){
+                context.fillStyle='tomato'
+                context.strokeStyle='white'
+                context.fillRect(x * _SIZEBLOCK,
+                    y * _SIZEBLOCK,
+                    _SIZEBLOCK,
+                    _SIZEBLOCK)
+                context.strokeRect(x * _SIZEBLOCK,
+                    y * _SIZEBLOCK,
+                    _SIZEBLOCK,
+                    _SIZEBLOCK)
+            }
+        }
+    }
+}
+
+showArea(game.area)
