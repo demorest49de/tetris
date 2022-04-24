5ff30970d966de5c5e570e2d66e3581390ef11e0
shevchenkoak <demorest49de@gmail.com>
2022-04-24 22:50:59 +0300

    c

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const _SIZEBLOCK = 30
-
+// todo peremestit' v game
+let previouslyPressedKey
 
 // mehanika
 
@@ -34,23 +35,71 @@ const game = {
             ['o', 'x', 'o'],
             ['o', 'x', 'o'],
             ['x', 'x', 'o'],
+        ],
+        rotationIndex: 0,
+        rotation: [
+            [
+                ['o', 'x', 'o'],
+                ['o', 'x', 'o'],
+                ['x', 'x', 'o'],
+            ],
+            [
+                ['o', 'o', 'o'],
+                ['x', 'x', 'x'],
+                ['o', 'o', 'x'],
+            ],
+            [
+                ['o', 'x', 'x'],
+                ['o', 'x', 'o'],
+                ['o', 'x', 'o'],
+            ],
+            [
+                ['x', 'o', 'o'],
+                ['x', 'x', 'x'],
+                ['o', 'o', 'o'],
+            ],
         ]
     },
 
     moveLeft() {
-        this.activeTetrаmino.x -= 1
+        if (this.checkOutPosition(this.activeTetrаmino.x - 1, this.activeTetrаmino.y)) {
+            this.activeTetrаmino.x -= 1
+        }
     },
 
     moveRight() {
-        this.activeTetrаmino.x += 1
+        if (this.checkOutPosition(this.activeTetrаmino.x + 1, this.activeTetrаmino.y)) {
+            this.activeTetrаmino.x += 1
+        }
     },
 
     moveDown() {
-        this.activeTetrаmino.y += 1
+        if (this.checkOutPosition(this.activeTetrаmino.x, this.activeTetrаmino.y + 1)) {
+            this.activeTetrаmino.y += 1
+        }else {
+            this.stopMove()
+        }
+    },
+
+    checkBorder(){
+        if (previouslyPressedKey === 'ArrowLeft' &&
+            !this.checkOutPosition(this.activeTetrаmino.x, this.activeTetrаmino.y)) {
+            this.activeTetrаmino.x += 1
+        }
+        else if(previouslyPressedKey === 'ArrowRight' &&
+            !this.checkOutPosition(this.activeTetrаmino.x, this.activeTetrаmino.y)){
+            this.activeTetrаmino.x -= 1
+        }
     },
 
     rotateTetromino() {
-
+        // console.log(previouslyPressedKey)
+        this.activeTetrаmino.rotationIndex =
+            this.activeTetrаmino.rotationIndex < 3 ?
+                this.activeTetrаmino.rotationIndex + 1 : 0
+        this.activeTetrаmino.block =
+            this.activeTetrаmino.rotation[this.activeTetrаmino.rotationIndex]
+        this.checkBorder()
     },
 
     get viewArea() {
@@ -68,8 +117,39 @@ const game = {
         return area
     },
 
-    checkOutPosition(x,y){
-        
+    checkOutPosition(x, y) {
+        const tetromino = this.activeTetrаmino.block
+
+        for (let i = 0; i < tetromino.length; i++) {
+            for (let j = 0; j < tetromino[i].length; j++) {
+                // фигура не проваливается в фигуру
+                if (tetromino[i][j] === 'o') continue
+                // фигура не проваливается в пол
+                console.log('x + j', +(x + j))
+                if (!this.area[y + i] ||
+                    // фигура не уходит слева и справа за границы стакана
+                    !this.area[y + i][x + j] ||
+                    // фигура не проваливается в фигуру
+                    this.area[y + i][x + j] === 'x'
+                ) {
+                    return false
+                }
+            }
+        }
+        return true
+    },
+
+    stopMove(){
+        const {x,y,block: tetrаmino} = this.activeTetrаmino
+
+        for (let i = 0; i < tetrаmino.length; i++) {
+            const row = tetrаmino[i]
+            for (let j = 0; j < row.length; j++) {
+                if (row[j] === 'x') {
+                    this.area[y + i][x + j] = tetrаmino[i][j]
+                }
+            }
+        }
     }
 }
 
@@ -86,7 +166,7 @@ canvas.height = _SIZEBLOCK * 20
 const context = canvas.getContext('2d')
 
 const showArea = (area) => {
-    context.clearRect(0,0,canvas.width, canvas.height)
+    context.clearRect(0, 0, canvas.width, canvas.height)
     for (let y = 0; y < area.length; y++) {
         const line = area[y]
 
@@ -112,14 +192,16 @@ const showArea = (area) => {
 // tak kak document vhodit v window
 window.addEventListener('keydown', e => {
     const key = e.code
-    switch (key) {
+        switch (key) {
         case 'ArrowLeft':
             game.moveLeft()
             showArea(game.viewArea)
+            previouslyPressedKey = key
             break
 
         case 'ArrowRight':
             game.moveRight()
+            previouslyPressedKey = key
             showArea(game.viewArea)
             break
 
@@ -128,7 +210,7 @@ window.addEventListener('keydown', e => {
             showArea(game.viewArea)
             break
 
-        case 'Space':
+        case 'ArrowUp':
             game.rotateTetromino()
             showArea(game.viewArea)
             break
